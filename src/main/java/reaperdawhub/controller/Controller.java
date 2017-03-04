d16adb7cc5bf68d05dea79ee60fdaf1459e8b533
CodingSpiderFox <sunrisechain@gmail.com>
2017-03-04 02:03:04 +0100

    automatically create schema on application startup add implementation for projectsService

--- src/main/java/reaperdawhub/controller/Controller.java
+++ src/main/java/reaperdawhub/controller/Controller.java
@@ -1,6 +1,5 @@
 package reaperdawhub.controller;
 
-import java.util.*;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 import reaperdawhub.persistence.model.Project;
-import reaperdawhub.persistence.model.ProjectFile;
 import reaperdawhub.service.ProjectsService;
 
 @RestController
@@ -24,8 +21,7 @@ public class Controller {
 
     @Autowired
     private ProjectsService projectsService;
-    
-    private static final String template = "Hello, %s!";
+
     private final AtomicLong counter = new AtomicLong();
     
     /*
@@ -36,24 +32,52 @@ public class Controller {
     */
     
     @RequestMapping("/projects")
-    public List<Project> getProjects(@RequestParam(value="name", defaultValue="World") String name) {
-        return new ArrayList<Project>();
+    public ResponseEntity<List<Project>> getProjects() {
+        List<Project> users = projectsService.findAllProjects();
+        if(users.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);//You many decide to return HttpStatus.NOT_FOUND
+        }
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
     
     @RequestMapping(value="/projects/{projectId}", method=RequestMethod.GET)
-    public Project getProject(@PathVariable("projectId") long projectId, Model model) {
-        return new Project(counter.incrementAndGet());
+    public ResponseEntity<Project> getProject(@PathVariable("projectId") long projectId, Model model) {
+        System.out.println("Fetching project with id " + projectId);
+        Project project = projectsService.findById(projectId);
+        if (project == null) {
+            System.out.println("Project with id " + projectId + " not found");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(project, HttpStatus.OK);
     }
     
     
     @RequestMapping(value="/projects/{projectId}", method=RequestMethod.DELETE)
-    public void deleteProject(@PathVariable("projectId") long projectId, Model model) {
-        return;
+    public ResponseEntity<Project> deleteProject(@PathVariable("projectId") long projectId, Model model) {
+        System.out.println("Fetching & Deleting project with id " + projectId);
+ 
+        Project user = projectsService.findById(projectId);
+        if (user == null) {
+            System.out.println("Unable to delete. Project with id " + projectId + " not found");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+ 
+        projectsService.deleteProjectById(projectId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
     
-    @RequestMapping(value="/projects/{projectId}", method=RequestMethod.PUT)
-    public void updateProject(@PathVariable("projectId") long projectId, Model model) {
-        return;
+    @RequestMapping(value = "/projects/{projectId}", method = RequestMethod.PUT)
+    public ResponseEntity<Project> updateUser(@PathVariable("projectId") long projectId, @RequestBody Project project) {
+        Project currentProject = projectsService.findById(projectId);
+         
+        if (currentProject == null) {
+            System.out.println("Project with id " + projectId + " not found");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+ 
+        currentProject.setName(project.getName());
+        projectsService.updateProject(project);
+        return new ResponseEntity<>(project, HttpStatus.OK);
     }
     
     
@@ -64,18 +88,14 @@ public class Controller {
 
       if (projectsService.doesProjectExist(project)) {
           System.out.println("A project with name " + project.getName() + " already exist");
-          return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+          return new ResponseEntity<>(HttpStatus.CONFLICT);
       }
 
       projectsService.saveProject(project);
 
       HttpHeaders headers = new HttpHeaders();
       headers.setLocation(ucBuilder.path("/projects/{id}").buildAndExpand(project.getId()).toUri());
-      return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+      return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
-      
-    @RequestMapping(value="/projects/{projectId}/projectFiles", method=RequestMethod.GET)
-    public List<ProjectFile> getProjectFiles(@PathVariable("projectId") long projectId, Model model) {
-        return new ArrayList<ProjectFile>();
-    }
+
 }
--- src/main/java/reaperdawhub/service/ProjectsServiceImpl.java
+++ src/main/java/reaperdawhub/service/ProjectsServiceImpl.java
@@ -1,91 +1,75 @@
 package reaperdawhub.service;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 import reaperdawhub.persistence.base.IDao;
-import reaperdawhub.persistence.dao.ProjectsDao;
 
 import reaperdawhub.persistence.model.Project;
 
 @Service("projectsService")
 @Transactional(isolation = Isolation.SERIALIZABLE)
 public class ProjectsServiceImpl implements ProjectsService {
-	
-        @Autowired    
-        private IDao<Project> projectsDao;
-    
-	private static final AtomicLong counter = new AtomicLong();
-	
-	private static List<Project> projects;
-	
-	static {
-		projects = populateDummyProjects();
-	}
 
-	public List<Project> findAllProjects() {
-		return projectsDao.getAll();
-	}
-	
-	public Project findById(long id) {
-		for(Project project : projects){
-			if(project.getId() == id){
-				return project;
-			}
-		}
-		return null;
-	}
-	
-	public Project findByName(String name) {
-		for(Project project : projects){
-			if(project.getName().equalsIgnoreCase(name)){
-				return project;
-			}
-		}
-		return null;
-	}
-	
-	public void saveProject(Project project) {
-		project.setId(counter.incrementAndGet());
-		projects.add(project);
-	}
+    @Autowired
+    private IDao<Project> projectsDao;
 
-	public void updateProject(Project project) {
-		int index = projects.indexOf(project);
-		projects.set(index, project);
-	}
+    private static final AtomicLong counter = new AtomicLong();
 
-	public void deleteProjectById(long id) {
-		
-		for (Iterator<Project> iterator = projects.iterator(); iterator.hasNext(); ) {
-		    Project project = iterator.next();
-		    if (project.getId() == id) {
-		        iterator.remove();
-		    }
-		}
-	}
+    private static List<Project> projects;
 
-	public boolean doesProjectExist(Project project) {
-		return findByName(project.getName())!=null;
-	}
+    public List<Project> findAllProjects() {
+        return projectsDao.getAll();
+    }
 
-	private static List<Project> populateDummyProjects(){
-		List<Project> projects = new ArrayList<Project>();
-		projects.add(new Project(counter.incrementAndGet()));
-		projects.add(new Project(counter.incrementAndGet()));
-		projects.add(new Project(counter.incrementAndGet()));
-		projects.add(new Project(counter.incrementAndGet()));
-		return projects;
-	}
+    public Project findById(long id) {
+        for (Project project : findAllProjects()) {
+            if (project.getId() == id) {
+                return project;
+            }
+        }
+        return null;
+    }
 
-	public void deleteAllProjects() {
-		projects.clear();
-	}
+    public Project findByName(String name) {
+        for (Project project : findAllProjects()) {
+            if (project.getName().equalsIgnoreCase(name)) {
+                return project;
+            }
+        }
+        return null;
+    }
+
+    public void saveProject(Project project) {
+        project.setId(counter.incrementAndGet());
+        projects.add(project);
+    }
+
+    public void updateProject(Project project) {
+        int index = projects.indexOf(project);
+        projects.set(index, project);
+    }
+
+    public void deleteProjectById(long id) {
+
+        for (Iterator<Project> iterator = findAllProjects().iterator(); iterator.hasNext();) {
+            Project project = iterator.next();
+            if (project.getId() == id) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public boolean doesProjectExist(Project project) {
+        return findByName(project.getName()) != null;
+    }
+
+    public void deleteAllProjects() {
+        projects.clear();
+    }
 
 }
